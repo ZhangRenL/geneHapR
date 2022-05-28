bbf45ac7711cd0f039498dca20b3808c22f90ab8
Zhangrenl <zhang_renliang@163.com>
2022-05-28 11:22:07 +0000

    update R/onload.R.

--- R/onload.R
+++ R/onload.R
@@ -1,5 +1,8 @@
 .onLoad <- function(libname, pkgname) {
+    message("")
     message("lastupdate: 2022.5.28")
+    message("Author: Zhang Ren Liang\n(Mail: zhang_renliang@163.com)")
+    message("")
     options(stringsAsFactors = FALSE)
 }
 
